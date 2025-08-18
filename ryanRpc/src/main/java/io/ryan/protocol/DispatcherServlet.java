ebe6a7543f1253da03d323eb040ee3695051d00e
ryan <wyrms01@outlook.com>
2025-08-18 18:35:43 +0800

    内嵌 tomcat 以及请求分发处理器初始化

---
+++ ryanRpc/src/main/java/io/ryan/protocol/DispatcherServlet.java
@@ -0,0 +1,17 @@
+package io.ryan.protocol;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class DispatcherServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        new HttpHandler().handler(req, resp);
+
+    }
+}
---
+++ ryanRpc/src/main/java/io/ryan/protocol/HttpHandler.java
@@ -0,0 +1,11 @@
+package io.ryan.protocol;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class HttpHandler {
+
+    public void handler(HttpServletRequest req, HttpServletResponse resp){
+
+    }
+}
