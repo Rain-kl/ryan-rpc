924001207eb391e92a8752649f4815e54e6d2cb7
ryan <wyrms01@outlook.com>
2025-08-18 18:35:07 +0800

    初始化工程

---
+++ Consumer/src/main/java/io/ryan/Main.java
@@ -0,0 +1,12 @@
+package io.ryan;
+
+import io.ryan.service.HelloService;
+
+//TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
+// 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
+public class Main {
+    public static void main(String[] args) {
+        HelloService helloService = new HelloService();
+        helloService.sayHello("Ryan");
+    }
+}
---
+++ Provider/src/main/java/io/ryan/Main.java
@@ -0,0 +1,15 @@
+package io.ryan;
+
+
+import io.ryan.protocol.HttpServer;
+
+//TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
+// 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
+public class Main {
+    public static void main(String[] args) {
+
+        HttpServer server = new HttpServer();
+        server.start("localhost", 8080);
+
+    }
+}
